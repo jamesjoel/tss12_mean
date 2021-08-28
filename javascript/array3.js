6980857a646e683ad5c347712a8da2664a11d1ad
jamesjoel <james.joelj@gmail.com>
2021-08-28 14:15:14 +0530

    json in javascript

---
+++ javascript/array3.js
@@ -0,0 +1,8 @@
+var data = [
+    "indore",
+    "mumbai",
+    "pune",
+    ["red", "green", "blue"]
+];
+
+console.log(data[3][1]);
---
+++ javascript/object2.js
@@ -0,0 +1,13 @@
+var data = {
+        name : "rohit",
+        age : 25,
+        city : "indore",
+        gender : "male",
+        family : {
+            name : "amar",
+            age : 25,
+            relation : "brother"
+        }
+    };
+console.log(data.family.name);
+
