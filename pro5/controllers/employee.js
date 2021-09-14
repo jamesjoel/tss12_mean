625127a5694235d9e26d9af618c8da35cb3125cd
jamesjoel <james.joelj@gmail.com>
2021-09-14 13:53:46 +0530

    add form

--- pro5/controllers/employee.js
+++ pro5/controllers/employee.js
@@ -2,40 +2,8 @@ var express = require("express");
 var routes = express.Router();
 
 routes.get("/", (req, res)=>{
-    var emp = [
-        {
-            name : "rohit",
-            age : 25,
-            city : "indore",
-            salary : 5000
-        },
-        {
-            name: "nilesh",
-            age: 20,
-            city: "pune",
-            salary: 7000
-        },
-        {
-            name: "amar",
-            age: 21,
-            city: "indore",
-            salary: 5500
-        },
-        {
-            name: "jaya",
-            age: 30,
-            city: "mumbai",
-            salary: 7800
-        },
-        {
-            name: "nidhi",
-            age: 34,
-            city: "delhi",
-            salary: 8000
-        }
-    ];
-    var pagedata = { emp : emp };
-    res.render("employee/index", pagedata);
+    
+    res.render("employee/index");
 })
 
 module.exports = routes;
