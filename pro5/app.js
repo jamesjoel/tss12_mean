0d9bdf216755b0c8ea6ab478f13cc410527a72c2
jamesjoel <james.joelj@gmail.com>
2021-09-15 14:04:23 +0530

    insert into mongodb

--- pro5/app.js
+++ pro5/app.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var app = express();
 
+
 var routes = require("./config/routes"); // "./config/routes" this is a file name/path.
 //  "routes" is a var name, can change also
 
 app.use(express.static(__dirname+"/assets"));
+
+// this code is used for express old version (4.15 and older)
+// var bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
 app.set("view engine", "ejs");
 app.use(routes);
 
--- pro5/controllers/employee.js
+++ pro5/controllers/employee.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var routes = express.Router();
 
+var MongoClient = require("mongodb").MongoClient;
+
 routes.get("/", (req, res)=>{
     
     res.render("employee/index");
 })
 
+
+routes.post("/save", (req, res)=>{
+    // console.log(req.body);
+    // MongoClient.connect(connection URL, callback fun)
+    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+        // use tss12
+        var db = con.db("tss12");
+        // db.student.insert({})
+        db.collection("employee").insertOne(req.body, ()=>{
+            // console.log("DONE-------------");
+            res.redirect("/employee");
+        });
+    });
+})
+
 module.exports = routes;
+
+/*
+    res.send()
+    res.sendFile()
+    res.render()
+    res.redirect()
+
+*/
