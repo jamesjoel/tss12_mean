0e3c16f52db6004c6f800e8458736ca805a9f5a6
jamesjoel <james.joelj@gmail.com>
2021-09-22 13:59:04 +0530

    test

--- mytest1/controllers/student.js
+++ mytest1/controllers/student.js
@@ -24,5 +24,54 @@ routes.post("/", (req, res)=>{
     })
 })
 
+routes.get("/delete/:a", (req, res)=>{
+    var id = req.params.a;
+    var objId = mongodb.ObjectId(id);
+    MongoClient.connect(mongoUrl, (err, con)=>{
+        var db = con.db("tss12");
+        db.collection("student").remove({ _id : objId }, ()=>{
+            res.redirect("/student");
+        })
+    })
+})
+
+routes.get("/edit/:a", (req, res)=>{
+    var id = req.params.a;
+    var objId = mongodb.ObjectId(id);
+    MongoClient.connect(mongoUrl, (err, con)=>{
+        var db = con.db("tss12");
+        db.collection("student").find({ _id : objId}).toArray((err, result)=>{
+            var pagedata = { result : result[0] };
+            res.render("student/edit", pagedata);
+        })
+    })
+})
+
+routes.post("/update/:a", (req, res)=>{
+    var id = req.params.a;
+    var objId = mongodb.ObjectId(id);
+    MongoClient.connect(mongoUrl, (err, con)=>{
+        var db = con.db("tss12");
+        db.collection("student").updateMany({_id : objId}, { $set : req.body }, ()=>{
+            res.redirect("/student");
+        })
+    })
+})
+
 
 module.exports = routes;
+/*
+
+db.coll.insert(obj, callback)
+
+db.coll.remove(obj, callback)
+
+db.coll.update(obj, {$set : obj}, callback)
+
+db.call.find().toArray(callback)
+
+
+
+
+
+*/
