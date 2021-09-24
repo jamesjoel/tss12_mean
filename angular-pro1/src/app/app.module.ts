00496fe9ab6e5f06a117c64a28031f4a6754a024
jamesjoel <james.joelj@gmail.com>
2021-09-24 13:39:52 +0530

    angular routing

--- angular-pro1/src/app/app.module.ts
+++ angular-pro1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { MiniBoxComponent } from './mini-box/mini-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent,
+    MiniBoxComponent
   ],
   imports: [
     BrowserModule,
